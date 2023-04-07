146d56ed37ac5b8a1d62aba928515dc27a3d1a20
M NIETO <mrconietoprez@gmail.com>
2023-04-07 21:42:14 +0200

    Reestructuración y traducción de la página

--- index.js
+++ index.js
@@ -12,8 +12,26 @@ window.onload = function() {
 
   // maximum number of slides
   maxSlide = slides.length - 1;
+
+  document.getElementById("slidetext").innerHTML=slidetexts.sl0;
+  document.getElementById("slidelink").innerHTML=slidelinktexts.sl0;
+  document.getElementById("slidelink").href=slidelinks.sl0;
 };
 
+slidetexts={
+ sl0: "Buscamos nuevos integrantes! Puedes acceder a la lista de oportunidades con sus respectivos requisitos y descripciones el siguiente enlace.",
+ sl1: "¡Nuestro equipo ha sido aceptado en el programa 'Generación espontánea' de la UPV! ¡Os esperamos a todos los que estéis interesados en formar parte!",
+}
+
+slidelinktexts={
+  sl0: "Consulta las vacantes",
+  sl1: "Echa un vistazo al resto de grupos",
+}
+
+slidelinks={
+  sl0: "sections/hiring/hiring.html",
+  sl1: "https://generacionespontanea.upv.es/grupos",
+}
 
 function nextsl() {
     // check if current slide is the last and reset current slide
@@ -27,6 +45,11 @@ function nextsl() {
     slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
     });
+
+    var object = `sl${curSlide}`
+    document.getElementById("slidetext").innerHTML=slidetexts[object];
+    document.getElementById("slidelink").innerHTML=slidelinktexts[object];
+    document.getElementById("slidelink").href=slidelinks[object];
 };
 
 
